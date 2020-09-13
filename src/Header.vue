6011b24511e5532ab1bea50c1f157b6b61e19a02
henry-chen-2020 <40145021+henry-chen-2020@users.noreply.github.com>
2020-09-12 18:19:38 -0700

    Create Header.vue

---
+++ src/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Bootcamp: Bookstore</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;  
+}
+</style>
